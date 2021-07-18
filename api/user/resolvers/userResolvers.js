481cca2c3bb2a41c34248e6407b00744e5bf25af
Marcelo Muniz da Luz <ma_celinho18@hotmail.com>
2021-07-18 00:04:56 -0300

    crud completo

--- api/user/resolvers/userResolvers.js
+++ api/user/resolvers/userResolvers.js
@@ -1,5 +1,15 @@
-const userResolvers = {
 
+const userResolvers = {
+    Query: {
+        users: (root, args, { dataSources }) => dataSources.usersAPI.getUsers(),
+        user: (root, { id }, { dataSources }) => dataSources.usersAPI.getUserByID(id)
+    },
+
+    Mutation: {
+        adicionaUser: async (root, user, { dataSources }) => dataSources.usersAPI.adicionaUser(user),
+        atualizaUser: async (root, novosDados, { dataSources }) => dataSources.usersAPI.atualizaUser(novosDados),
+        deletaUser: async (root, { id }, { dataSources }) => dataSources.usersAPI.deletaUser(id)
+    }
 };
 
 module.exports = userResolvers;
